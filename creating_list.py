5362f4807a79b743bb7ae63b39ebad00c543d2c7
andreybannikov <andrewnnov@yandex.ru>
2019-06-17 22:16:15 +0300

    list

---
+++ creating_list.py
@@ -0,0 +1,9 @@
+prices = []
+temp = [32.0, 212.0, 0.0, 81.6, 100.0, 45.3]
+print(temp)
+words = ['hello','world']
+car_details = ['Toyota', 'Rav4', 2.2, 60807]
+everything = [prices, temp, words, car_details]
+odds_and_ends = [[1,2,3], ['a','b', 'c'], ['One', 'Two', 'Three']
+         
+
