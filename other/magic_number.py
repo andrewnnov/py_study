ce55b15a81949d47ee9162adcc9571e59b3de5c2
andreybannikov <andrewnnov@yandex.ru>
2019-06-30 07:43:13 +0300

    fix our method

--- other/magic_number.py
+++ other/magic_number.py
@@ -1,23 +1,24 @@
 import random
 
+magic_numbers = [random.randint(0,9),random.randint(0,9)]
+
 
 def ask_user_and_check_number():
     user_number = int(input("Enter a number between 0 and 9: "))    
     if user_number in magic_numbers:
-        print("You got the number right!")
+        return "You got the number right!"
     if user_number not in magic_numbers:
-        print("You did't quite get it.")
+        return "You did't quite get it."
 
-
-magic_numbers = [random.randint(0,9),random.randint(0,9)]
 
 def run_program_x_times(chances):
     for attempt in range(chances): #range(chances) is [0,1,2]
         print("This is attempt {} ".format(attempt+1))
-        ask_user_and_check_number()
+        print(ask_user_and_check_number())
+        
 
 user_attempts = int(input("Enter a number attempts: "))
-run_program_x_times(user_attempt)
+run_program_x_times(user_attempts)
         
   
     
