b81ddeaa0dfdb3dfc537422272498311b581ae64
andreybannikov <andrewnnov@yandex.ru>
2019-07-07 10:49:41 +0300

    add value to dictionary

--- app.py
+++ app.py
@@ -6,3 +6,19 @@ student = {"name": "Jose",
            }}
 
 print(student['exams']['final'])
+
+
+def create_student():
+    # ask the user for the student's name
+    # Create the dictionary in the format {'name': '<student_name>, 'marks': []}
+    # return that dictionary
+    name = input("Please enter the new student's name: ")
+    student_data = {
+        'name': name,
+        'marks': []
+    }
+    return student_data
+
+
+print(create_student())
+
