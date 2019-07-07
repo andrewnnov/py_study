b74edda933340b45e25477359348307316f6d33b
andreybannikov <andrewnnov@yandex.ru>
2019-07-07 10:36:57 +0300

    test dictionary

---
+++ app.py
@@ -0,0 +1,8 @@
+student = {"name": "Jose",
+           "mark": [20, 40, 70, 80, 99],
+           "exams": {
+               "final": 70,
+               "midterm": 50
+           }}
+
+print(student['exams']['final'])
